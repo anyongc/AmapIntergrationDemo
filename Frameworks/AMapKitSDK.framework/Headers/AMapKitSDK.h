9bc49f93396cddfda798944d4d7169bd93ca39b4
An Yongchao, (Yongchao.An@partner.bmw.com) <Yongchao.An@partner.bmw.com>
2018-06-29 15:59:04 +0800

    add framework

---
+++ Frameworks/AMapKitSDK.framework/Headers/AMapKitSDK.h
@@ -0,0 +1,19 @@
+//
+//  AMapKitSDK.h
+//  AMapKitSDK
+//
+//  Created by Guo Longxiang, (Longxiang.Guo@partner.bmw.com) on 6/28/18.
+//  Copyright © 2018 Guo Longxiang, (Longxiang.Guo@partner.bmw.com). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMapKitSDK.
+FOUNDATION_EXPORT double AMapKitSDKVersionNumber;
+
+//! Project version string for AMapKitSDK.
+FOUNDATION_EXPORT const unsigned char AMapKitSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMapKitSDK/PublicHeader.h>
+
+
